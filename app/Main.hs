769a5d9ac53c1e61167c6a9269da3a44bfbb8026
Veronika Gorbatovskaia <v.gorbatovskaia@innopolis.university>
2025-07-12 06:35:40 +1000

    To do generation random words in the game process

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,13 @@
 module Main (main) where
 
+import Control.Monad.IO.Class (liftIO)
+import Configuration.Dotenv (defaultConfig, loadFile)
+import WordList (getRandomWord)
 import Control.Applicative
 import Data.Char
 import Data.Text as T
 import System.Environment (getEnv)
-import Configuration.Dotenv (defaultConfig, loadFile)
-import Telegram.Bot.API ()
+import Telegram.Bot.API
 import qualified Telegram.Bot.API as Telegram
 import Telegram.Bot.Simple
 import Telegram.Bot.Simple.Debug
@@ -13,8 +15,13 @@ import Telegram.Bot.Simple.UpdateParser
 
 data GameState
   = NotPlaying
-  | WaitingForAnswer Text
+  | WaitingForAnswer Text Int
   deriving (Show)
+
+data AnswerResult
+  = InvalidInput Text
+  | WrongAnswer Text
+  | CorrectAnswer Text
 
 data Model = Model
   { gameState :: GameState
@@ -26,6 +33,7 @@ data Action
   | Help
   | StartGame
   | StopGame
+  | StartGameWithWord Text
   | TextMessage Text
   deriving (Show)
 
@@ -52,43 +60,88 @@ bot =
       case action of
         Start ->
           model <# do
-            replyText (pack "hello this is bot")
+            reply $
+              (toReplyMessage (pack "Hello, i'm WordleBot! To start the game type /game."))
+                { replyMessageReplyMarkup = Just (SomeReplyKeyboardMarkup wordleKeyboard)
+                }
         Help ->
           model <# do
-            replyText (pack "this is help")
+            replyText
+              ( pack
+                  "Wordle is game in which you need to guess the word by receiving feedback on letters. \n\
+                  \🟩 - letter is present in right position \n\
+                  \🟨 - letter is present \n\
+                  \⬛ - no such letter \n\
+                  \Available commands: \n\
+                  \/game - to start the game \n\
+                  \/stop - to stop the game \n\
+                  \/help - inforamtion about commands \n\
+                  \/start - to restart bot."
+              )
         StartGame ->
-          model {gameState = WaitingForAnswer (pack "apple")} <# do
-            replyText (pack "new game")
+          model <# liftIO (StartGameWithWord <$> getRandomWord)
+        StartGameWithWord word ->
+          model {gameState = WaitingForAnswer word 5} <# do
+            replyText (pack "Guess the word!")
         StopGame ->
           case gameState model of
-            WaitingForAnswer _ ->
+            WaitingForAnswer _ _ ->
               model {gameState = NotPlaying} <# do
-                replyText (pack "game finished")
+                replyText (pack "Game finished.")
             NotPlaying ->
               model <# do
-                replyText (pack "you are not in game")
+                replyText (pack "You are not in game.")
         TextMessage msg ->
           case gameState model of
             NotPlaying ->
               model <# do
                 replyText (pack "I do not understand")
-            WaitingForAnswer word ->
-              if T.toLower word /= T.toLower msg
-                then
+            WaitingForAnswer word cnt ->
+              case validateAnswer (T.toLower word) (T.toLower msg) of
+                InvalidInput feedback ->
                   model <# do
-                    replyText (validateAnswer word msg)
-                else
-                  model {gameState = NotPlaying} <# do
-                    replyText (validateAnswer word msg)
+                    replyText feedback
+                WrongAnswer feedback ->
+                  let new_cnt = cnt - 1
+                   in if new_cnt <= 0
+                        then
+                          model {gameState = NotPlaying} <# do
+                            reply $
+                              (toReplyMessage (feedback <> pack "\nYou lost, no more tries."))
+                                {replyMessageReplyMarkup = Just (SomeReplyKeyboardMarkup wordleKeyboard)}
+                        else
+                          model {gameState = WaitingForAnswer word new_cnt} <# do
+                            replyText (feedback <> pack "\nTries left " <> pack (show new_cnt) <> pack "/5")
+                CorrectAnswer feedback ->
+                  model <# do
+                    reply $
+                      (toReplyMessage (feedback <> pack "\nCongrats you won"))
+                        {replyMessageReplyMarkup = Just (SomeReplyKeyboardMarkup wordleKeyboard)}
 
-validateAnswer :: Text -> Text -> Text
+    wordleKeyboard :: ReplyKeyboardMarkup
+    wordleKeyboard =
+      ReplyKeyboardMarkup
+        { replyKeyboardMarkupKeyboard =
+            [ [ KeyboardButton (pack "/game") Nothing Nothing Nothing Nothing Nothing Nothing,
+                KeyboardButton (pack "/help") Nothing Nothing Nothing Nothing Nothing Nothing
+              ]
+            ],
+          replyKeyboardMarkupResizeKeyboard = Just True,
+          replyKeyboardMarkupOneTimeKeyboard = Just True,
+          replyKeyboardMarkupSelective = Nothing,
+          replyKeyboardMarkupInputFieldSelector = Nothing,
+          replyKeyboardMarkupIsPersistent = Nothing
+        }
+
+validateAnswer :: Text -> Text -> AnswerResult
 validateAnswer correct guessed
-  | not (T.all isLetter guessed) = pack "only letters are allowed"
-  | T.length guessed /= 5 = pack "word should be 5 letters" <> correct
-  | otherwise = answerFeedBack correct guessed
+  | not (T.all isLetter guessed) = InvalidInput (pack "Only letters are allowed")
+  | T.length guessed /= 5 = InvalidInput (pack "Word should be 5 letters")
+  | guessed == correct = CorrectAnswer (answerFeedBack correct guessed)
+  | otherwise = WrongAnswer (answerFeedBack correct guessed)
 
 answerFeedBack :: Text -> Text -> Text
-answerFeedBack correct guessed = T.unlines [T.toUpper letterLine, emojiLine]
+answerFeedBack correct guessed = emojiLine
   where
     charFeedBack :: Char -> Char -> Char
     charFeedBack c g
@@ -96,10 +149,6 @@ answerFeedBack correct guessed = T.unlines [T.toUpper letterLine, emojiLine]
       | g `T.elem` correct = '🟨'
       | otherwise = '⬛'
 
-    centerUnderEmoji :: Char -> Text
-    centerUnderEmoji c = T.concat [pack "  ", T.singleton c, pack "  "]
-
-    letterLine = T.concat $ Prelude.map centerUnderEmoji (T.unpack guessed)
     emojiLine = T.zipWith charFeedBack correct guessed
 
 run :: Telegram.Token -> IO ()
